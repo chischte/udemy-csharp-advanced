0e29a40bc09942bfb7558b40cd978bf5a633ec22
michi <chischte@gmx.ch>
2020-10-01 21:52:44 +0200

    Initial commit of created Visual Studio Solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace udemy_csharp_advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
