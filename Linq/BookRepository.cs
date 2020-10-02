92124f52a52cbbc3f82fe991ae5987e51685f0ba
michi <chischte@gmx.ch>
2020-10-02 15:46:58 +0200

    Make first linq examples

---
+++ Linq/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "Book 1 st", Price = 1.1f},
+                new Book() {Title = "Book 3 rd", Price = 3},
+                new Book() {Title = "Book 5 th", Price = 8},
+                new Book() {Title = "Book 6 th", Price = 20},
+                new Book() {Title = "Book 4 th", Price = 3},
+                new Book() {Title = "Book 2 nd", Price = 22}
+            };
+        }
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Linq
 {
@@ -6,7 +7,34 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var books = new BookRepository().GetBooks();
+
+            // 1st use of Linq
+            var cheapBooks = books.Where(b => b.Price < 10);
+            foreach(var book in cheapBooks)
+            {
+                Console.WriteLine("Bookprice < 10 only: " + book.Title + " Price:  " + book.Price);
+            }
+
+            // 2nd use of Linq
+            books.OrderBy(b => b.Title);
+            foreach (var book in books)
+            {
+                Console.WriteLine("Sorted by title: "+ book.Title + " " + book.Price);
+            }
+
+            // Chain linqs and select string only
+            var cheapBooksTitles = books
+                .Where(b => b.Price < 10)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title); // Select string-only
+
+            foreach(var book in cheapBooksTitles)
+            {
+                Console.WriteLine("String only : " + book);
+            }
+
+
         }
     }
 }
