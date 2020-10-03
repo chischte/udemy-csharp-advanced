b8f4abfaace55e6baa896d3f003b593d119295cf
michi <chischte@gmx.ch>
2020-10-03 14:46:52 +0200

    Complete lecture 12 "Nullable Types"

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<DateTime> date = null;
+
+            Console.WriteLine("GetValueOrDefault(): " + date.GetValueOrDefault());
+            Console.WriteLine("HasValue: " + date.HasValue);
+            Console.WriteLine("Value: " + date.Value);
+
+            DateTime? dateNullable = new DateTime(2014, 1, 1);
+            DateTime date2 = dateNullable.GetValueOrDefault();
+
+            DateTime date3 = dateNullable ?? DateTime.Today;
+        }
+    }
+}
