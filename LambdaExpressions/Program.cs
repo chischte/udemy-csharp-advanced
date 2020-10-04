51c92b8f586678ff082902ce72272cb8e9347f77
michi <chischte@gmx.ch>
2020-10-04 12:30:35 +0200

    Complete lecture "lambda expressions"

--- LambdaExpressions/Program.cs
+++ LambdaExpressions/Program.cs
@@ -6,6 +6,7 @@ namespace LambdaExpressions
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine(Square(7));
 
             Func<int, int> squareLambda = number => number * number;
@@ -14,11 +15,40 @@ namespace LambdaExpressions
             const int factor = 5;
             Func<int, int> multiplier = n => n * factor;
             Console.WriteLine(multiplier(10));
-       
+            */
+
+            
+            var books = new BookRepository().GetBooks();
+            
+            // Find cheap books without a lambda expression:
+            
+            var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+            // Find cheap books using a lambda expression:
+
+            var cheapBooksLambda = books.FindAll(b => b.Price < 10);
+            foreach (var book in cheapBooksLambda)
+            {
+                Console.WriteLine("Lambda Result: "+ book.Title);
+            }
+
+
+
+
         }
+        /*
         static int Square(int number)
         {
             return number * number;
+        }
+        */
+        static bool IsCheaperThan10Dollars(Book book)
+        {
+            return book.Price < 10;
         }
     }
 }
