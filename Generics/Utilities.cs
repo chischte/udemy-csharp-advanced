78e03a300bf4b7bd8e8474094de20e98a1203af7
michi <chischte@gmx.ch>
2020-10-04 11:15:22 +0200

    BKP

--- Generics/Utilities.cs
+++ Generics/Utilities.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace Generics
 {
-    // where T : IComparable --> implement an interface
-    // where T : Product     --> 
-    // where T : struct      --> value type
-    // where T : class       --> reference Type
-    // where T : new()       --> has a default constructor
+    // where T : IComparable   --> implement (or be) an interface
+    // where T : ClassName     --> derive from (or be) a class
+    // where T : struct        --> value type or a structure
+    // where T : class         --> reference Type
+    // where T : new()         --> has a default constructor
     public class Utilities<T> where T : IComparable, new()
     {
         public T Max(T a, T b)
