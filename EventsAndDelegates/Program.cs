9fd678d44db4304109d4c19950ea3b4178489bcd
michi <chischte@gmx.ch>
2020-11-09 10:43:53 +0100

    Start chapter events and delegates

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "The Big Lebowsky" };
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService();    //subscriber 1
+            var messageService = new MessageService(); // subscriber 2
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded; // Add reference to subscriber
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    class VideoEncoder
+    {
+        // 1. Define a delgate:
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+        
+        // 2. Define an event based on that delegate:
+        public event VideoEncodedEventHandler VideoEncoded;
+
+        // 3. Raise the event
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...{0}", video.Title);
+            Thread.Sleep(2000);
+
+            OnVideoEncoded();
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, EventArgs.Empty);
+            }
+        }
+    }
+}
